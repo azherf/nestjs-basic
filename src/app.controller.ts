bb6787eb051e88ca6cd27b059f26a9dab327c27b
Azher Farooqi <azher@momentum.io>
2025-01-06 20:20:27 -0800

    chore: Add a root router per controller, and define new routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 
-@Controller()
+@Controller("/app")
 export class AppController {
-  @Get()
+  @Get("/hello")
   getRootRoute(): string {
     return "Hello World!";
-  }  
+  }
+
+  @Get("/goodbye")
+  getGoodbyeRoute(): string {
+    return "Goodbye!";
+  }
 }
